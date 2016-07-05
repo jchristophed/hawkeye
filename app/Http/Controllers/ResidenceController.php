3a2a7f6de4e7bce7132389c469d86c2f31b831ba
Jean-Christophe Duvivier <jean-christophe.duvivier@jalis.fr>
2016-07-05 14:05:31 +0200

    1.1.22 - correction calcul logements
    - correction du nombre de logements vides non reloues sur la page d accueil

--- app/Http/Controllers/ResidenceController.php
+++ app/Http/Controllers/ResidenceController.php
@@ -36,7 +36,7 @@ class ResidenceController extends Controller
 
             $residence->setNbFlats($this->flatRepository->getNbFlats($residence->id));
             $residence->setNbIncompleteContracts($this->contractRepository->getNbIncompleteContracts($residence->id));
-            $residence->setNbFreeFlats($this->flatRepository->getNbFreeFlats($residence->id));
+            $residence->setNbFreeFlats($this->flatRepository->getNbFreeFlatsAndFuture($residence->id));
         }
 
         return view('residence.listing', ['residences' => $residences]);
